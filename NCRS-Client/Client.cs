18598cd3e434b3a168238d547b5580e23cfef455
KevinYakha <kevin@yakha.eu>
2024-06-03 20:06:00 +0200

    Added methods to find the issuer, complaint location and a method to insert a new complaint

--- NCRS-Client/Client.cs
+++ NCRS-Client/Client.cs
@@ -7,17 +7,69 @@ namespace NCRS_Client;
 
 class Client : HttpClient
 {
-    public async static Task<HttpResponseMessage> SubmitNewComplaintAsync(Complaint newComplaint)
+    private Uri _uri = new("https://localhost:port");
+
+    public async Task<HttpResponseMessage> SubmitNewComplaintAsync(Complaint newComplaint)
     {
         try
         {
             HttpClient http = new();
-            Uri uri = new(new("https://localhost:port"), "CreateNewComplaint");
+            Uri uri = new(_uri, "CreateNewComplaint");
 
             HttpRequestMessage message = new(HttpMethod.Post, uri);
             message.Content = JsonContent.Create(newComplaint);
 
             return await http.SendAsync(message);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<Tenant> FindTenantByNameAsync(Tenant tenant)
+    {
+
+        try
+        {
+            HttpClient http = new();
+            Uri uri = new(_uri, "RetrieveTenantByName");
+
+            HttpRequestMessage message = new(HttpMethod.Get, uri);
+            message.Content = JsonContent.Create(tenant);
+            HttpResponseMessage response = await http.SendAsync(message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Tenant>();
+            }
+
+            return new();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<Apartment> FindApartment(Apartment apartment)
+    {
+
+        try
+        {
+            HttpClient http = new();
+            Uri uri = new(_uri, "RetrieveApartment");
+
+            HttpRequestMessage message = new(HttpMethod.Get, uri);
+            message.Content = JsonContent.Create(apartment);
+            HttpResponseMessage response = await http.SendAsync(message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Apartment>();
+            }
+
+            return new();
         }
         catch (Exception)
         {
--- NCRS-Client/MainWindow.xaml.cs
+++ NCRS-Client/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using NCRS_API.Data;
 
 namespace NCRS_Client
 {
@@ -16,9 +11,94 @@ namespace NCRS_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Client _httpClient = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < (int)Complaint.ComplaintCategory.COUNT; i++)
+            {
+                cb_category.Items.Add((Complaint.ComplaintCategory)i);
+            }
+        }
+
+        private void tb_apartment_nr_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void bt_submit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Complaint newComplaint = new()
+                {
+                    Issuer = new() { FirstName = tb_first_name.Text, LastName = tb_last_name.Text },
+                    ComplaintLocation = new() { ApartmentNr = int.Parse(tb_apartment_nr.Text) },
+                    Description = tb_description.Text,
+                    Category = (Complaint.ComplaintCategory)int.Parse(cb_category.Text)
+                };
+
+                HttpResponseMessage response;
+                Task.Run(() =>
+                {
+                    Dispatcher.InvokeAsync(async () =>
+                    {
+                        response = await _httpClient.SubmitNewComplaintAsync(newComplaint);
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void bt_find_issuer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Tenant tenant = new()
+                {
+                    FirstName = tb_first_name.Text,
+                    LastName = tb_last_name.Text
+                };
+                Task.Run(() =>
+                {
+                    Dispatcher.InvokeAsync(async () =>
+                    {
+                        await _httpClient.FindTenantByNameAsync(tenant);
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void bt_find_location_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Apartment apartment = new()
+                {
+                    ApartmentNr = int.Parse(tb_apartment_nr.Text)
+                };
+                Task.Run(() =>
+                {
+                    Dispatcher.InvokeAsync(async () =>
+                    {
+                        await _httpClient.FindApartment(apartment);
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
