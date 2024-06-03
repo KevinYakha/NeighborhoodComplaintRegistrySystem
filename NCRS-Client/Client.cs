b207e9306831f78c26651a64dbeb3075cdcbf727
KevinYakha <kevin@yakha.eu>
2024-06-03 12:17:24 +0200

    Added HttpClient

---
+++ NCRS-Client/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+
+using NCRS_API.Data;
+
+namespace NCRS_Client;
+
+class Client : HttpClient
+{
+    public async static Task<HttpResponseMessage> SubmitNewComplaintAsync(Complaint newComplaint)
+    {
+        try
+        {
+            HttpClient http = new();
+            Uri uri = new(new("https://localhost:port"), "CreateNewComplaint");
+
+            HttpRequestMessage message = new(HttpMethod.Post, uri);
+            message.Content = JsonContent.Create(newComplaint);
+
+            return await http.SendAsync(message);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
